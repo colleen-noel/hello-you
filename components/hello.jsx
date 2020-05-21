1e378359bc566a53b65dc606251987e70a6d7a2a
Colleen Noel <noel.colleen@gmail.com>
2020-05-21 14:04:40 -0400

    answer

---
+++ components/hello.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+  <div>
+    <div className="title">Colleen Noel</div>
+    <div className="about">I am a web development student looking to become a front-end developer. After working for thirteen years in healthcare, I've decided to take on new challenges in a career where I can use my creative side.</div>
+  </div>
+)
---
+++ index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Name from './components/hello'
+
+render(<Name />, document.getElementById('root')
+)
